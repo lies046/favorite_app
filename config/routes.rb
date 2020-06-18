52238b53a8c64e6a0f1089cfdc52be29a04a3003
lies046 <skytree.skysky@gmail.com>
2020-06-18 19:51:05 +0900

    Add Recipe

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'home#top'
   devise_for :users
   resource :user, except: [:new, :create, :destroy]
+  resources :recipes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
