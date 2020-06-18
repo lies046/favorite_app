b972d5c4524afac8daf98f404059a20e386cab74
lies046 <skytree.skysky@gmail.com>
2020-06-18 20:15:22 +0900

    Add Errors and Form Views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -22,6 +22,25 @@ class RecipesController < ApplicationController
   end
 
   def edit
+    @recipe = Recipe.find(params[:id])
+    if @recipe.user_id != current_user.id
+      redirect_to recipes_path, alert: "不正なアクセスです。"
+    end
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path(@recipe), notice: "更新に成功しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    redirect_to recipes_path
   end
 
   private
